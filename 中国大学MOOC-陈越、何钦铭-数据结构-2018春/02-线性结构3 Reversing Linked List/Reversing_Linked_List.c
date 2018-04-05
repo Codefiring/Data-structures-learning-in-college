538b1adca37652651fe1758ec71ff792f74717e3
codefiring <hhx970127@163.com>
2018-04-05 21:41:45 +0800

    2

---
+++ 中国大学MOOC-陈越、何钦铭-数据结构-2018春/02-线性结构3 Reversing Linked List/Reversing_Linked_List.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define Max_N 10000
+
+typedef struct LinkedNode* LinkedList;
+typedef struct LinkedNode
+{
+	int index;
+	LinkedList Pre;
+	LinkedList Next;
+};
+
+
+
+int main()
+{
+
+}
