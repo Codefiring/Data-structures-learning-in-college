8a6a9ba9f063b5df3f01042e705053a3b5181528
codefiring <hhx970127@163.com>
2018-04-23 13:49:54 +0800

    add the first page

--- 数据结构-严蔚敏版/线性表的链式表示/Link_list.h
+++ 数据结构-严蔚敏版/线性表的链式表示/Link_list.h
@@ -1,27 +1,29 @@
 #ifndef LINK_LIST_H_
 #define LINK_LIST_H_
+#define LIST_INIT_SIZE  100
 
 #include<stdlib.h>
 #include<malloc.h>
 #include "Status.h"
 
+
 typedef struct LNode {	//linknode type
 	Elemtype data;
 	struct LNode * next;
-}*Link, *Position;
+}LNode, *Link, *Position;
 
 typedef struct {	//linklist type
 	Link head, tail;	//define the head LNode and the tail LNode
 	int len;
 }LinkList;
 
-Status MakeNode(Link *p, Elemtype e) {
+Status MakeNode(Link p, Elemtype e) {
 	//make node point to e, return ERROR if failed
-	p = (Link)malloc(sizeof(Link));
-	printf("%d", sizeof(Link));
-	if(!p)	
-		return(ERROR);
-	p->data = e;
+	
+	//p = (Link)malloc(sizeof(Link));
+	(*p).data = e;
+	(*p).next = NULL;
+
 	return OK;
 }//MakeNode
 
@@ -33,32 +35,78 @@ void FreeNode(Link *p) {
 
 Status InitList(LinkList *l) {
 	//make an empty linklist
-	Link p;
+	Link p;		//get a new linknode 
 	p = (Link)malloc(sizeof(Link));
 	if (!p)
 		return ERROR;
+	p->data = 0;
 	p->next = NULL;
-	l.head = p;
-	l.tail = l.head;
-	l.len = 0;
+	l->head = p;		//make linklist first point to this linknode
+	l->tail = l->head;
+	l->len = 0;
 	return OK;
 }
 
-Status DestroyList(LinkList l) {
+Status DestroyList(LinkList *l) {
+	//destroy the list.the list will be nonexist.
 	Link p;
-	while (l.head!=l.tail) {
-		p = l.head->next;
-		free(l.head);
-		l.head = p;
+	while (l->head!=l->tail) {
+		p = l->head->next;
+		FreeNode(l->head);
+		l->head = p;
 	}
-	free(l.head);
-	l.head = NULL;
-	l.tail = NULL;
+	FreeNode(l->head);
+	l->head = NULL;
+	l->tail = NULL;
 	return OK;
 }
 
-Status ClearList(LinkList l) {
+Status ClearList(LinkList *l) {
+//	LinkList p;
+	return OK;
 
+}
+
+Status InsFirst(LinkList *l, Link s) {
+	//Give the linklist pointer l and the node pointer s
+	//put s before the head of l
+	s->next = l->head->next;
+	l->head->next = s;
+	while (l->tail->next != NULL)l->tail = l->tail->next;
+	l->len++;
+
+	return OK;
+}
+
+Status DelFirst(LinkList *l, Link q) {
+	//Give the linklist pointer l and the node pointer q
+	//Delete l->head and return it as q
+	if (!l->len)return ERROR;
+	(*q).data = l->head->next->data;
+	(*q).next = NULL;
+	l->head->next = l->head->next->next;
+	l->len--;
+	return OK;
+}
+
+Status Append(LinkList *l, Link s) {
+	
+	l->tail->next = s;
+	while (l->tail->next != NULL) {
+		l->tail = l->tail->next;
+		l->len++;
+	}
+	return OK;
+}
+
+Status Remove(LinkList *l, Link q) {
+	Link tmp;
+	tmp = l->head;
+	while (tmp->next != l->tail)tmp = tmp->next;
+	q = l->tail;
+	l->tail = tmp;
+	l->tail->next = NULL;
+	return OK;
 }
 
 
--- 数据结构-严蔚敏版/线性表的链式表示/linkedlist.c
+++ 数据结构-严蔚敏版/线性表的链式表示/linkedlist.c
@@ -2,7 +2,70 @@
 #include "Link_list.h"
 
 int main() {
-	LinkList l;
-	printf("%d\n", sizeof(l));
+	LinkList list;
+	LinkList *l = &list;
+
+	printf("Creating LinkList: \"list\"...\n");
+	
+	InitList(l);
+	printf("list.len:%d\n", list.len);
+	printf("After InitList: address of list.head:%p\n", list.head);
+	printf("After InitList: address of list.tail:%p\n", list.tail);
+
+	LNode tmp;
+	Link s = &tmp;
+	MakeNode(s, 5);
+
+	printf("%d\n", tmp.data);
+
+	InsFirst(l, s);
+	printf("After InFirst: list.head->next->data = %d\n", list.head->next->data);
+	printf("After InFirst: list.len = %d\n", list.len);
+	printf("After InFirst: address of list.head:%p\n", list.head);
+	printf("After InFirst: address of list.tail:%p\n", list.tail);
+
+	LNode cnode;
+	Link node_chain =  &cnode;
+	cnode.next = NULL;
+	const Link node_head = node_chain;
+	
+	for (int i = 0; i < 10; i++) {
+		node_chain = node_head;
+		while (node_chain->next != NULL)node_chain = node_chain->next;
+		Link s_tmp;
+		s_tmp = (Link)malloc(sizeof(Link));
+		MakeNode(s_tmp, i);
+		node_chain->next = s_tmp;
+	}
+
+	Append(l, node_head);
+	printf("After Append: list.len = %d\n", list.len);
+	printf("After Append: address of list.head:%p\n", list.head);
+	printf("After Append: address of list.tail:%p\n", list.tail);
+	printf("After Append: list.head->next->data = %d\n", list.head->data);
+	printf("After Append: list.tail->data = %d\n", list.tail->data);
+
+	LNode q_tmp;
+	Link q = &q_tmp;
+	DelFirst(l, q);
+	printf("After DelFirst: q->data = %d\n", q->data);
+	printf("After DelFirst: list.head->next->data = %d\n", list.head->next->data);
+	printf("After DelFirst: list.tail->data = %d\n", list.tail->data);
+	printf("After DelFirst: list.len = %d\n", list.len);
+
+	LNode q2_tmp;
+	Link q2 = &q2_tmp;
+	Remove(l, q2);
+	printf("After Remove: q->data = %d\n", q->data);
+	printf("After Remove: list.head->next->data = %d\n", list.head->next->data);
+	printf("After Remove: list.tail->data = %d\n", list.tail->data);
+	printf("After Remove: list.len = %d\n", list.len);
+
+
+
+
+	//DestroyList(l);
+	//printf("list.head:%p\n", l);
+
 	return 0;
 }
