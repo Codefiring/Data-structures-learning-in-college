ba291ff0fa6e6b93dc0125a339c7f333a685a738
codefiring <hhx970127@163.com>
2018-05-10 01:02:33 +0800

    factorial

---
+++ Data Structures, Algorithms, & Applications in C++, 2nd Edition/factorial.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+    if(n>1)
+        return n*factorial(n-1);
+    else return 1;
+}
+
+int main(){
+    int a = 4;
+    int fact = 0;
+    fact = factorial(a);
+    cout<<fact<<endl;
+    return 0;
+}
---
+++ Data Structures, Algorithms, & Applications in C++, 2nd Edition/linearList.h
@@ -0,0 +1,18 @@
+#ifndef LINEAR_LIST_H
+#define LINEAR_LIST_H
+
+template<class T>
+class linearList
+{
+public:
+	virtual ~linearList() {};
+	virtual bool empty() const = 0;
+	virtual int size() const = 0;
+	virtual T& get(int theIndex) const = 0;
+	virtual int indexOf(const T& theElement)const = 0;
+	virtual void erase(int theIndex, const T& theElement) = 0;
+	virtual void insert(int theIndex, const T& theElement) = 0;
+	virtual void output(ostream& out)const = 0;
+};
+
+#endif
